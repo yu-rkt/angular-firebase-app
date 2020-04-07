4871755af9360f313abec60fa8ed7a2a6bab706a
yu728 <yuu.watanabe0728@gmail.com>
2020-04-07 11:29:46 +0900

    fix(auth): AngularFireAuth

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth/auth';
-import { auth } from 'firebase';
+import { auth, User } from 'firebase';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  afUser$: Observable<User> = this.afAuth.user;
+
   constructor(private afAuth: AngularFireAuth) {}
 
   login() {
-    this.afAuth.auth.signInWithPopup(new auth.GithubAuthProvider());
+    this.afAuth.signInWithPopup(new auth.GithubAuthProvider());
   }
 
   logout() {}
