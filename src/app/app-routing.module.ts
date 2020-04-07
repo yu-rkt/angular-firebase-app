1bb130609f7923e568bc9565d808dd075eb74f35
yu728 <yuu.watanabe0728@gmail.com>
2020-04-07 14:12:58 +0900

    feat(auth): guardの追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+    canLoad: [AuthGuard],
+    canActivate: [AuthGuard],
   },
   {
     path: 'welcome',
@@ -16,6 +19,8 @@ const routes: Routes = [
     path: 'create',
     loadChildren: () =>
       import('./create/create.module').then((m) => m.CreateModule),
+    canLoad: [AuthGuard],
+    canActivate: [AuthGuard],
   },
 ];
 
