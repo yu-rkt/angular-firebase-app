748d2057d583b35062673c4c22525d7a3daeabe2
yu728 <yuu.watanabe0728@gmail.com>
2020-04-09 17:34:42 +0900

    feat(exp.pipe): 経験値表示パイプ追加

--- functions/src/git-hook.function.ts
+++ functions/src/git-hook.function.ts
@@ -5,16 +5,34 @@ admin.initializeApp();
 
 const db = admin.firestore();
 
+const expTable = [20, 40, 100, 250, 500, 1000, 1500, 4000, 10000];
+
+const EARNED_EXP = 10;
+
 export const gitHook = functions.https.onRequest(async (request, response) => {
   const pets = await db
     .collection('pets')
     .where('ownerGitHubId', '==', request.body.sender.id)
     .get();
 
-  const increment = admin.firestore.FieldValue.increment(10);
-  pets.docs.forEach((docs) =>
-    docs.ref.update({
+  const pet = pets.docs[0].data();
+
+  let level = 1;
+
+  expTable.some((nextExp) => {
+    if (pet.exp + EARNED_EXP >= nextExp) {
+      level++;
+      return false;
+    } else {
+      return true;
+    }
+  });
+
+  const increment = admin.firestore.FieldValue.increment(EARNED_EXP);
+  pets.docs.forEach((doc) =>
+    doc.ref.update({
       exp: increment,
+      level: level,
     })
   );
   response.send('success!');
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,9 +6,10 @@ import { HomeComponent } from './home/home.component';
 import { PetComponent } from './pet/pet.component';
 
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { ExpPipe } from '../exp.pipe';
 
 @NgModule({
-  declarations: [HomeComponent, PetComponent],
+  declarations: [HomeComponent, PetComponent, ExpPipe],
   imports: [CommonModule, HomeRoutingModule, MatProgressBarModule],
 })
 export class HomeModule {}
